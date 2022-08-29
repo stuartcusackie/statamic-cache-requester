884d6761daa168fe17ac662d629f9e79d97c27ff
Stuart Cusack <stuart@stuartcusack.ie>
2022-08-29 17:52:58 +0100

    Update statamic-glide-requester.php

--- config/statamic-glide-requester.php
+++ config/statamic-glide-requester.php
@@ -7,7 +7,7 @@ return [
     | Source attributes
     |--------------------------------------------------------------------------
     |
-    | This option define what html attributes to look for 
+    | This option defines what html attributes to look for 
     | on each img / picture element. If you are using lazy loading then
     | you might have different attributes than the ones listed below.
     |
@@ -25,8 +25,8 @@ return [
     | Asset views
     |--------------------------------------------------------------------------
     |
-    | These options is useful when you are requesting single asset views.
-    | For example, a lightbox component might request a larger
+    | These options are useful when you are requesting single asset views.
+    | For example, a lightbox component might use JS to request a larger
     | size for a responsive image, in which case the values below might be
     |
     | 'post_data_attributes' => [
@@ -35,7 +35,9 @@ return [
     |    
     | 'asset_view_path' => '/lightbox-image-asset'
     |
-    | This feature assumes you are POSTing the asset id to the url.
+    | The script will then search each element for 'data-asset-id'
+    | and POST it as data to lightbox-image-asset to return your
+    | custom view.
     |
     */
 
